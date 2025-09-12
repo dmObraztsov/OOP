4a0840ab7f329e918c7992a2dde513869bc70fda
dmObraztsov <d.obraztsov@g.nsu.ru>
2025-09-13 00:30:05 +0700

    1-1

--- Task_1_1/src/test/java/heapsort/AppTest.java
+++ Task_1_1/src/test/java/heapsort/AppTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AppTest {
 
