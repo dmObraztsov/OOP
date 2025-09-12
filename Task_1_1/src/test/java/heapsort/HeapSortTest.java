8f67c8e9a5fdc955db9078651dfa4206f0f8822c
dmObraztsov <d.obraztsov@g.nsu.ru>
2025-09-13 00:33:19 +0700

    1-1

--- Task_1_1/src/test/java/heapsort/HeapSortTest.java
+++ Task_1_1/src/test/java/heapsort/HeapSortTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class HeapSortTest {
 
