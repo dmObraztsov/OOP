3b90c305c8d4f2a7b6fd991ffeeb3641bb5fec8d
dmObraztsov <d.obraztsov@g.nsu.ru>
2025-09-13 01:15:29 +0700

    1_1_1

---
+++ Task_1_1_1/src/test/java/HeapSortTest.java
@@ -0,0 +1,60 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+class HeapSortTest {
+
+    @Test
+    void handlesEmpty() {
+        int[] a = {};
+        HeapSort.sort(a);
+        assertArrayEquals(new int[]{}, a);
+    }
+
+    @Test
+    void singleElement() {
+        int[] a = {42};
+        HeapSort.sort(a);
+        assertArrayEquals(new int[]{42}, a);
+    }
+
+    @Test
+    void duplicates() {
+        int[] a = {5, 1, 5, 3, 5, 2};
+        int[] ref = a.clone();
+        Arrays.sort(ref);
+        HeapSort.sort(a);
+        assertArrayEquals(ref, a);
+    }
+
+    @Test
+    void alreadySorted() {
+        int[] a = {-3, -1, 0, 2, 4, 9};
+        int[] ref = a.clone();
+        HeapSort.sort(a);
+        assertArrayEquals(ref, a);
+    }
+
+    @Test
+    void reverseSorted() {
+        int[] a = {9, 4, 2, 0, -1, -3};
+        int[] ref = a.clone();
+        Arrays.sort(ref);
+        HeapSort.sort(a);
+        assertArrayEquals(ref, a);
+    }
+
+    @Test
+    void randomLargeEqualsJdkSort() {
+        Random rnd = new Random(7);
+        int[] a = rnd.ints(200_000, -1_000_000, 1_000_000).toArray();
+        int[] ref = a.clone();
+        Arrays.sort(ref);
+        HeapSort.sort(a);
+        assertArrayEquals(ref, a);
+    }
+}
