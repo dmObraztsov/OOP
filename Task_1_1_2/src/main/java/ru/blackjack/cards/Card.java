6708ac07be0d4c165ab07846a8280fb4afd07c63
dmObraztsov <d.obraztsov@g.nsu.ru>
2025-09-19 03:24:46 +0700

    Task_1_1_2

--- Task_1_1_2/src/main/java/ru/blackjack/cards/Card.java
+++ Task_1_1_2/src/main/java/ru/blackjack/cards/Card.java
@@ -2,30 +2,49 @@ package ru.blackjack.cards;
 
 import java.util.Objects;
 
-
 /**
- *
+ * Карта игральной колоды.
  */
 public final class Card {
 
     private final Suit suit;
     private final Rank rank;
 
+    /**
+     * Создаёт карту с указанными мастью и достоинством.
+     *
+     * @param suit масть карты
+     * @param rank достоинство карты
+     */
     public Card(Suit suit, Rank rank) {
         this.suit = Objects.requireNonNull(suit);
         this.rank = Objects.requireNonNull(rank);
     }
 
+    /**
+     * Возвращает масть карты.
+     *
+     * @return масть
+     */
     public Suit getSuit() {
         return suit;
     }
 
+    /**
+     * Возвращает достоинство карты.
+     *
+     * @return достоинство
+     */
     public Rank getRank() {
         return rank;
     }
 
+    /**
+     * Строит отображаемое имя карты без значения очков.
+     *
+     * @return строка вида «Дама Пики»
+     */
     public String buildDisplayNameWithoutValue() {
         return rank.getRussianName() + " " + suit.getRussianName();
     }
-
 }
