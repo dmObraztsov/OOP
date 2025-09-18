4ce100002bafd57cdd3ef2ab6c4be45e42bee7b3
dmObraztsov <d.obraztsov@g.nsu.ru>
2025-09-19 02:33:58 +0700

    1_1_1 changes after rewiew (modifiers, constructor, order of methods, Main class)

--- Task_1_1_1/src/test/java/HeapSortTest.java
+++ Task_1_1_1/src/test/java/HeapSortTest.java
@@ -1,9 +1,8 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import java.util.Arrays;
 import java.util.Random;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 
 class HeapSortTest {
 
